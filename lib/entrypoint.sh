fb3dbfa951ad021bcd33c484f1738de41ca72c55
Erik Sandberg <erik@repairtechsolutions.com>
2022-02-16 10:32:53 -0800

    added comment

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-gem install activesupport -v 4.2
 
 gem install rubocop -v 1.25.0
 gem install rubocop-rspec -v 2.8.0
 gem install rubocop-performance -v 1.13.2
-gem install rubocop-rails -v 2.13.2
 gem install rubocop-rake -v 0.6.0
+
+# activesupport required for rubocop-rails
+gem install activesupport -v 4.2
+gem install rubocop-rails -v 2.13.2
 
 ruby /action/lib/index.rb
